36a7edec5c25e6285bbe280f4cf0d36c4d7eaa8d
André Augusto <andre.augusto@trela.com.br>
2022-09-23 19:10:46 -0300

    fix start-spark script

--- scripts/start-spark.sh
+++ scripts/start-spark.sh
@@ -3,11 +3,11 @@
 if [ "$SPARK_MODE" == "master" ];
 then
     export SPARK_MASTER_HOST=`hostname`
-    cd /opt/spark/sbin && ./start-master.sh -h $SPARK_MASTER_HOST --port $SPARK_MASTER_PORT --webui-port $SPARK_MASTER_WEBUI_PORT
+    cd ${SPARK_HOME}/sbin && ./start-master.sh -h $SPARK_MASTER_HOST --port $SPARK_MASTER_PORT --webui-port $SPARK_MASTER_WEBUI_PORT
 
 elif [ "$SPARK_MODE" == "worker" ];
 then
-    cd /opt/spark/sbin && ./start-worker.sh --webui-port $SPARK_WORKER_WEBUI_PORT $SPARK_MASTER_URL
+    cd ${SPARK_HOME}/sbin && ./start-worker.sh --webui-port $SPARK_WORKER_WEBUI_PORT $SPARK_MASTER_URL
 else
     echo "Undefined Workload Type $SPARK_MODE, must specify: master, worker"
 fi
