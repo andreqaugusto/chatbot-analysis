b4b01ebbc1126712b11e723a0ab95773df178238
André Augusto <andre.augusto@trela.com.br>
2022-10-12 22:32:32 -0300

    cleaning some env vars

--- scripts/start-spark.sh
+++ scripts/start-spark.sh
@@ -4,12 +4,12 @@ cd $SPARK_HOME/sbin
 
 if [ "$SPARK_MODE" == "master" ];
 then
-    export SPARK_MASTER_HOST=`hostname`
-    ./start-master.sh -h $SPARK_MASTER_HOST --port $SPARK_MASTER_PORT --webui-port $SPARK_MASTER_WEBUI_PORT
+
+    ./start-master.sh
 
 elif [ "$SPARK_MODE" == "worker" ];
 then
-    ./start-worker.sh --webui-port $SPARK_WORKER_WEBUI_PORT $SPARK_MASTER_URL
+    ./start-worker.sh $SPARK_MASTER_URL
 else
     echo "Undefined workload type $SPARK_MODE. Please specify one of those: master, worker"
 fi
